4b9affc389cb60ba3d2ca997adba2766df129fcf
Mais <mais.alheraki@gmail.com>
2025-04-12 04:06:45 +0300

    feat: add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "WiDS Dhahran @ KFUPM",
-  description: "Join us for the WiDS Dhahran @ KFUPM event, a global conference featuring inspiring talks and workshops in data science and AI.",
+  title: 'WiDS Dhahran @ KFUPM',
+  description:
+    'Join us for the WiDS Dhahran @ KFUPM event, a global conference featuring inspiring talks and workshops in data science and AI.',
+  openGraph: {
+    images: [{ url: './og-image.png' }],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: [{ url: './og-image.png' }],
+  },
 };
 
 export default function RootLayout({
