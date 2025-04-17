d47b2408d967029f81823b5bd3efb450283ebfde
Mais <mais.alheraki@gmail.com>
2025-04-17 09:30:57 +0300

    feat: update map

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -8,7 +8,7 @@ export const ContactSection = () => {
           {/* Google Maps - Compact Embed */}
           <div className="lg:col-span-2 rounded-xl overflow-hidden shadow-lg h-64 md:h-80">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.5939689526235!2d50.15082907604455!3d26.30725967701327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49e644c711d83b%3A0x89d26475360b1fb7!2sBuilding%2060%20-%20King%20Fahd%20Conference%20Center!5e0!3m2!1sen!2ssa!4v1744336657088!5m2!1sen!2ssa"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.402829716481!2d50.146740376531895!3d26.31345247700934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49e77b8b385991%3A0x371eff2334f61c0a!2sBuilding%2070%20-%20Auditorium!5e0!3m2!1sen!2ssa!4v1744871407117!5m2!1sen!2ssa"
               width="100%"
               height="100%"
               style={{ border: 0 }}
